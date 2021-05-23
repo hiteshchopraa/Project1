73f413f173aeef9bbc9ec1198bbf6a8e266b404d
Hitesh <59359401+hiteshchopraa@users.noreply.github.com>
2021-05-24 00:14:42 +0530

    Add files via upload

---
+++ java 2/music/src/main/java/com/accolite/music/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.accolite.music.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping(value="/test")
+	public String testAPI() {
+		return "test success";
+	}
+}
